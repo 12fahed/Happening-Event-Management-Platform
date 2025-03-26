c867545defcbf896fa7c070ca99451c39ff89ca1
Fahed Khan <127182880+12fahed@users.noreply.github.com>
2025-03-26 23:06:52 +0530

    Initial Commit

---
+++ src/app/test/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { ThreeDCardDemo } from "@/components/AccernityCard"
+
+export default function TestPage(){
+
+  return(
+    <div>
+      This is a Test Page
+      <ThreeDCardDemo />
+    </div>
+  )
+
+}
